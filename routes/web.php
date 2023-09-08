2aae03c886fcf83e91893194c74900fadbc4d772
Erick molina <erickmolina2002>
2023-09-07 22:27:43 -0300

    feat: layout

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function () {
+    return view('stocks\add');
+});
 
 Route::controller(StocksController::class)->group(function () {
     Route::get('/stocks', 'index')->name('stocks.index');
